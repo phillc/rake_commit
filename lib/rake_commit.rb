de584d674fd46bf1efc6cfe54559776c10a46f29
phillc <spyyderz@gmail.com>
2010-06-07 17:35:57 -0400

    removed appearingly unused methods

--- lib/rake_commit.rb
+++ lib/rake_commit.rb
@@ -9,27 +9,12 @@ module RakeCommit
     cruise_status.pass? ? true : are_you_sure?( "Build FAILURES: #{cruise_status.failures.join(', ')}" )
   end
 
-  def git_commit_with_message
-    commit_message = CommitMessage.new
-    sh_with_output("git config user.name #{commit_message.pair.inspect}")
-    message = "#{commit_message.feature} - #{commit_message.message}"
-    sh_with_output("git commit -m #{message.inspect}")
-  end
-
-  def are_you_sure?(message)
+  def self.are_you_sure?(message)
     puts "\n", message
     input = ""
     while (input.strip.empty?)
       input = Readline.readline("Are you sure you want to check in? (y/n): ")
     end
     return input.strip.downcase[0,1] == "y"
-  end
-
-  def sh_with_output(command)
-    puts command
-    output = `#{command}`
-    puts output
-    raise unless $?.success?
-    output
   end
 end
